b2be50eac1c361406b30c47f6ade65d87a998e6c
Lucas <lucas.yvernaux@gmail.com>
2023-10-17 18:16:42 +0200

    change database

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => 'https://www.db4free.net/phpMyAdmin/index.php',
+            'url' => 'mysql://sql11654071:xNTsRgmmjs@sql11.freemysqlhosting.net:3306/sql11654071',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
