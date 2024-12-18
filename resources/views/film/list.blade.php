4c76e0cb0a379d850afabc604aca4369a03e653a
Lucas <lucas.yvernaux@gmail.com>
2024-12-18 14:36:13 +0100

    fix: update size of main page

--- resources/views/film/list.blade.php
+++ resources/views/film/list.blade.php
@@ -10,18 +10,21 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <h1 class="text-center font-semibold">Liste des films des Yvernaux</h1>
-                    <a class="text-right block" href="{{ route('film.add') }}">add</a>
+                    @auth
+                        <a class="text-right block" href="{{ route('film.add') }}">add</a>
+                    @endauth
                     <form method="post" action="{{ route('film.search') }}">
                         @csrf
                         <label for="search">Rechercher :</label>
-                        <input type="text" name="search" value="{{ !empty($search) ? $search : '' }}" />
+                        <input class="w-4/5" type="text" name="search"
+                            value="{{ !empty($search) ? $search : '' }}" />
                         <input type="submit" class="bg-indigo-50 border-red-300" />
                     </form>
 
                     @if (isset($search) && !$movies->isEmpty())
                         <p style="font-style: italic">Resultat pour '{{ $search }}'</p>
                     @else
-                        <a class="text-center block" href="?tri=true"> Trier par ordre Alphabetique</a>
+                        <a class="text-center block hidden" href="?tri=true"> Trier par ordre Alphabetique</a>
                     @endif
 
 
